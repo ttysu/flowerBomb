ad64d8ccf5c86fef77aec8b500a4ca8216e0cd57
ttysu <32983924+ttysu@users.noreply.github.com>
2018-01-12 14:28:52 +0800

    Update monteCarloCalc.js

--- js/monteCarloCalc.js
+++ js/monteCarloCalc.js
@@ -28,7 +28,7 @@
                 //胜率
                 chance: (chance * 100).toFixed(2) + '%',
                 //期望收益
-                expect: ((chance * (this.players - 1) * 4 - (1 - chance) * 2) * 50).toFixed(2) + '%'
+                expect: ((chance * (this.players - 1) * 2 - (1 - chance) * 4) * 25).toFixed(2) + '%'
             }
         },
         init: function (pokerGroup, players) {
